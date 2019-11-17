6faf59ab2f72fb0206efe556c7ad296837a065ca
Vincent Letard <letard.vincent@gmail.com>
2019-11-17 09:46:20 -0500

    Added hashcode and equals for Factor.

--- analogy/matrix/Factor.java
+++ analogy/matrix/Factor.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Factor<E> {
-
   private final List<E> B;
   private final List<E> C;
   private final boolean crossed;
@@ -37,5 +36,40 @@ public class Factor<E> {
   
   public boolean isCrossed() {
     return this.crossed;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((B == null) ? 0 : B.hashCode());
+    result = prime * result + ((C == null) ? 0 : C.hashCode());
+    result = prime * result + (crossed ? 1231 : 1237);
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Factor<E> other = (Factor<E>) obj;
+    if (B == null) {
+      if (other.B != null)
+        return false;
+    } else if (!B.equals(other.B))
+      return false;
+    if (C == null) {
+      if (other.C != null)
+        return false;
+    } else if (!C.equals(other.C))
+      return false;
+    if (crossed != other.crossed)
+      return false;
+    return true;
   }
 }
